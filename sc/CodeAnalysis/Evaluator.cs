19650d93858c19ae11b7d6bf28213ae0d8e61ef1
Nils <herrnielsson.nd@gmail.com>
2021-02-21 12:54:41 +0100

    unary operator evaluation (1/2)

--- sc/CodeAnalysis/Evaluator.cs
+++ sc/CodeAnalysis/Evaluator.cs
@@ -26,6 +26,17 @@ namespace Santuryu.CodeAnalysis
                 return (int)n.LiteralToken.Value;
             }
 
+            if (node is UnaryExpressionSyntax u)
+            {
+                var operand = EvaluateExpression(u.Operand);
+                if (u.OperatorToken.Kind == SyntaxKind.PlusToken)
+                    return operand;
+                else if (u.OperatorToken.Kind == SyntaxKind.MinusToken)
+                    return -operand;
+                else
+                    throw new Exception($"Unexpected unary operator: {u.OperatorToken.Kind}");
+            }
+
             if (node is BinaryExpressionSyntax b)
             {
                 var left = EvaluateExpression(b.Left);
--- sc/CodeAnalysis/SyntaxKind.cs
+++ sc/CodeAnalysis/SyntaxKind.cs
@@ -17,6 +17,7 @@ namespace Santuryu.CodeAnalysis
         //Expressions
         LiteralExpression,
         BinaryExpression,
+        UnaryExpression,
         ParenthesizedExpression
     }
 }
