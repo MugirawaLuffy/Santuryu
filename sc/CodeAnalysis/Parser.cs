f4058f84b60099ab29200319dec96c1098148f48
Nils <herrnielsson.nd@gmail.com>
2021-02-21 12:43:21 +0100

    Move precedence to SyntaxFacts.cs

--- sc/CodeAnalysis/Parser.cs
+++ sc/CodeAnalysis/Parser.cs
@@ -98,7 +98,7 @@ namespace Santuryu.CodeAnalysis
             var left = ParsePrimaryExpression();
             while (true)
             {
-                var precedence = GetBinaryOperatorPrecedence(Current.Kind);
+                var precedence = Current.Kind.GetBinaryOperatorPrecedence();
                 if (precedence == 0 || precedence <= parentPrecedence)
                     break;
 
@@ -109,21 +109,7 @@ namespace Santuryu.CodeAnalysis
 
             return left;
         }
-        private static int GetBinaryOperatorPrecedence(SyntaxKind kind)
-        {
-            switch (kind)
-            {
-                case SyntaxKind.StarToken:
-                case SyntaxKind.SlashToken:
-                    return 2;
-                case SyntaxKind.PlusToken:
-                case SyntaxKind.MinusToken:
-                    return 1;
 
-                default:
-                    return 0;
-            }
-        }
 
         private ExpressionSyntax ParsePrimaryExpression()
         {
