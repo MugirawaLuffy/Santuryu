d62b7c1a11573c5fd0dc4b822a22f294190919f4
Nils <herrnielsson.nd@gmail.com>
2021-02-23 19:02:58 +0100

    primitive unit test for lexer tokens

--- Santuryu.Tests/CodeAnalysis/Syntax/LexerTest.cs
+++ Santuryu.Tests/CodeAnalysis/Syntax/LexerTest.cs
@@ -1,14 +1,36 @@
 using System;
+using System.Collections.Generic;
+using Santuryu.CodeAnalysis.Syntax;
 using Xunit;
+
 
 namespace Santuryu.Tests.CodeAnalysis.Syntax
 {
     public class LexerTest
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [MemberData(nameof(GetTokensData))]
+        public void Lexer_Lexes_Token(SyntaxKind kind, string text)
         {
+            var tokens = SyntaxTree.ParseTokens(text);
 
+            var token = Assert.Single(tokens);
+            Assert.Equal(text, token.Text);
+        }
+
+        public static IEnumerable<object[]> GetTokensData()
+        {
+            foreach (var t in GetTokens())
+                yield return new object[] { t.kind, t.text };
+
+        }
+        private static IEnumerable<(SyntaxKind kind, string text)> GetTokens()
+        {
+            return new[]
+            {
+                (SyntaxKind.IdentifierToken, "a"),
+                (SyntaxKind.IdentifierToken, "abc"),
+            };
         }
     }
 }
