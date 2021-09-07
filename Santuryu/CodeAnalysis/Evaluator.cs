8aca191adeb028d4de505d50b3e899de6da0ee9a
Nils <herrnielsson.nd@gmail.com>
2021-09-07 10:55:17 +0100

    New Builtin function Rnd for random numbers

--- Santuryu/CodeAnalysis/Evaluator.cs
+++ Santuryu/CodeAnalysis/Evaluator.cs
@@ -10,6 +10,7 @@ namespace Santuryu.CodeAnalysis
     {
         private readonly BoundBlockStatement _root;
         private readonly Dictionary<VariableSymbol, object> _variables;
+        private Random _random;
 
         private object _lastValue;
 
@@ -202,6 +203,14 @@ namespace Santuryu.CodeAnalysis
                 var message = (string)EvaluateExpression(node.Arguments[0]);
                 Console.WriteLine(message);
                 return null;
+            }
+            else if (node.Function == BuiltinFunctions.Rnd)
+            {
+                var max = (int)EvaluateExpression(node.Arguments[0]);
+                if (_random == null)
+                    _random = new Random();
+
+                return _random.Next(max);
             }
             else
             {
--- Santuryu/CodeAnalysis/Symbols/BuiltinFunctions.cs
+++ Santuryu/CodeAnalysis/Symbols/BuiltinFunctions.cs
@@ -10,6 +10,7 @@ namespace Santuryu.CodeAnalysis.Symbols
     {
         public static readonly FunctionSymbol Print = new FunctionSymbol("Print", ImmutableArray.Create(new ParameterSymbol("text", TypeSymbol.String)), TypeSymbol.Void);
         public static readonly FunctionSymbol Input = new FunctionSymbol("Input", ImmutableArray<ParameterSymbol>.Empty, TypeSymbol.String);
+        public static readonly FunctionSymbol Rnd = new FunctionSymbol("Rnd", ImmutableArray.Create(new ParameterSymbol("max", TypeSymbol.Int)), TypeSymbol.Int);
 
         internal static IEnumerable<FunctionSymbol> GetAll()
             => typeof(BuiltinFunctions).GetFields(BindingFlags.Public | BindingFlags.Static)
